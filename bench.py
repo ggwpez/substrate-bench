aa8e91dc54b16be56b1a45787047a611904b1196
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-01-11 11:28:34 +0100

    Update bench.py
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- bench.py
+++ bench.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
 import sys
+import os
 
 base_cmd="cargo run --profile=%s --features=runtime-benchmarks --manifest-path=bin/node/cli/Cargo.toml --quiet -- benchmark --chain=dev --execution=wasm --wasm-execution=compiled --steps=50 --repeat=20 --raw"
 
@@ -11,14 +12,17 @@ def parse_args():
 	parser.add_argument('--cwd', type=str, help='Substrate root directory', default=".")
 	return parser.parse_args()
 
-def list_benches(cmd):
-	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def list_benches(cmd, cwd):
+	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
 	stdout, stderr = p.communicate()
+	# check the exit code of the process.
+	if p.returncode != 0:
+		raise Exception("Rust:\n\n%s" % stderr.decode('utf-8'))
 	print(" " + "\n ".join(stdout.decode('utf-8').splitlines()[1:]))
 
 def run_benches(cmd, f, cwd):
 	print(cmd)
-	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, , cwd=cwd)
+	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
 	for line in p.stderr:
 		line = line.decode('utf-8')
 		if "Running Benchmark:" in line:
@@ -30,17 +34,20 @@ def run_benches(cmd, f, cwd):
 def run_pallet(pallet, profile, f, cwd):
 	cmd = (base_cmd % profile) + " --pallet=%s --extrinsic='*'" % pallet
 	print("%s: %s.*" % (profile, pallet))
-	list_benches(cmd + " --list")
+	list_benches(cmd + " --list", cwd)
 	run_benches(cmd, f, cwd)
 
 def main():
-	subprocess.run("mkdir -p results", shell=True)
 	args = parse_args()
+	if not os.path.isfile(os.path.join(args.cwd, "Cargo.toml")):
+		raise Exception("Cargo.toml not found. Use --cwd to specify the root directory of Substrate.")
+
+	subprocess.run("mkdir -p results", shell=True)
 	for profile in args.profiles:
 		fname = "results/%s.txt" % profile
 		with open(fname, 'w') as f:
 			for pallet in args.pallets:
-				run_pallet(pallet, profile, f, args.root)
+				run_pallet(pallet, profile, f, args.cwd)
 			print("Wrote to: %s\n" % fname)
 
 if __name__ == '__main__':
